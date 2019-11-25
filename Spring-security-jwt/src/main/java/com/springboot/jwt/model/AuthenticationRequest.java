1e7c50c8cab8a5ce1a40d54e0819ad10ceb2cd34
Ashish Manjhi <ashishmanjhi.97@gmail.com>
2019-11-25 10:53:51 +0530

    Spring-security-src

---
+++ Spring-security-jwt/src/main/java/com/springboot/jwt/model/AuthenticationRequest.java
@@ -0,0 +1,32 @@
+package com.springboot.jwt.model;
+
+import java.io.Serializable;
+
+public class AuthenticationRequest implements Serializable {
+    private String username;
+    private String password;
+
+    public AuthenticationRequest(){
+    }
+
+    public AuthenticationRequest(String username,String password){
+        this.setUsername(username);
+        this.setPassword(password);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
