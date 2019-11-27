116b85dfb4eccc3d7bfb2d7bfeeb817822d8b6a9
Ashish Manjhi <ashishmanjhi.97@gmail.com>
2019-11-27 17:06:29 +0530

    Spring-boot-kafka-consumer

---
+++ spring-boot-kafka-consumer/src/main/java/springboot/kafka/consumer/listener/KafkaConsumer.java
@@ -0,0 +1,21 @@
+package springboot.kafka.consumer.listener;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import springboot.kafka.consumer.model.User;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "Kafka_Example", groupId = "group_id")
+    public void consume(String message) {
+        System.out.println("Consumed message: " + message);
+    }
+
+
+    @KafkaListener(topics = "Kafka_Example_json", groupId = "group_json",
+            containerFactory = "userKafkaListenerFactory")
+    public void consumeJson(User user) {
+        System.out.println("Consumed JSON Message: " + user);
+    }
+}
